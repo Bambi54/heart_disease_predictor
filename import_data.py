5e8448e1ef6fcef74c790143a2378a6f2a2bbdaa
Aleksander Babij <aleksander.babij@gmail.com>
2024-11-08 21:53:03 +0100

    fixed import

--- import_data.py
+++ import_data.py
@@ -2,5 +2,5 @@ from ucimlrepo import fetch_ucirepo
 import pandas as pd
 
 def fetch_dataset():
-    data = fetch_ucirepo(id=697)
-    return pd.concat([data.features, data.targets], axis=1)
+    uciml_data = fetch_ucirepo(id=697)
+    return pd.concat([uciml_data.data.features, uciml_data.data.targets], axis=1)
