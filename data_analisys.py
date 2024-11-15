f3a1f480f3215b4586697382d5a63158f9a85fc8
Aleksander Babij <aleksander.babij@gmail.com>
2024-11-15 19:40:27 +0100

    created eda report

---
+++ data_analisys.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ydata_profiling import ProfileReport
+from ucimlrepo import fetch_ucirepo
+
+predict_students_dropout_and_academic_success = fetch_ucirepo(id=697)
+
+X = predict_students_dropout_and_academic_success.data.features
+y = predict_students_dropout_and_academic_success.data.targets
+
+data = pd.concat([X, y], axis=1)
+
+print("Informacje o danych:")
+data.info()
+
+print("\nPodgląd danych:")
+print(data.head())
+
+missing_data = data.isnull().sum()
+print("\nLiczba brakujących wartości w każdej kolumnie:")
+print(missing_data)
+
+print("\nStatystyki opisowe danych:")
+print(data.describe(include="all"))
+
+# Rozkłady zmiennych numerycznych
+plt.figure(figsize=(10, 6))
+data.hist(bins=20, figsize=(15, 10), color='blue', alpha=0.7)
+plt.suptitle('Histogramy zmiennych numerycznych')
+plt.show()
+
+
+# Rozkłady zmiennych kategorycznych
+categorical_columns = data.select_dtypes(include=['object']).columns
+for col in categorical_columns:
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x=data[col])
+    plt.title(f'Rozkład zmiennej kategorycznej: {col}')
+    plt.show()
+
+
+# Macierz korelacji
+plt.figure(figsize=(12, 10))
+numeric_data = data.select_dtypes(include=['float64', 'int64'])
+sns.heatmap(numeric_data.corr(), annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("Macierz korelacji zmiennych numerycznych")
+plt.show()
+
+# Generowanie raportu z Pandas Profiling
+profile = ProfileReport(data, title="Raport Pandas Profiling", explorative=True)
+profile.to_file("EDA_report.html")
