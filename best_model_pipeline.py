043d5eaea490d0bef9ca9909e059a94d103861a4
Aleksander Babij <aleksander.babij@gmail.com>
2024-11-15 20:14:31 +0100

    updated best model pipeline

--- best_model_pipeline.py
+++ best_model_pipeline.py
@@ -5,12 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from tpot.export_utils import set_param_recursive
+from import_data import fetch_dataset
+from sklearn.metrics import classification_report, accuracy_score
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
-tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1)
+tpot_data = fetch_dataset()
+features = tpot_data.drop('Target', axis=1)
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'], random_state=42)
+            train_test_split(features, tpot_data['Target'], random_state=42)
 
 # Average CV score on the training set was: 0.7829569023919956
 exported_pipeline = make_pipeline(
@@ -22,3 +24,9 @@ set_param_recursive(exported_pipeline.steps, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
+
+print("Raport klasyfikacji:")
+print(classification_report(testing_target, results))
+
+accuracy = accuracy_score(testing_target, results)
+print(f"\nDokładność modelu na zbiorze testowym: {accuracy:.2f}")
