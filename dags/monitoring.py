cdf91290b1747d165ccda0a047e4634e1b156665
Aleksander Babij <aleksander.babij@gmail.com>
2024-12-06 23:06:07 +0100

    smtp through gmail setup

--- dags/monitoring.py
+++ dags/monitoring.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.email import EmailOperator
 from datetime import datetime
 from airflow.utils.email import send_email
+
 
 
 def monitor_model(params: dict):
@@ -20,16 +20,14 @@ def monitor_model(params: dict):
     y_pred = model.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
-        
     if accuracy < 0.8:
         subject = "Warning: student_dropouts model accuracy dropped below threshold"
-        body = (
-            f"The model's accuracy dropped to {accuracy:.2f}, "
-            "Please investigate and retrain the model if necessary."
-        )
+        body = f'''The model's accuracy dropped to {accuracy:.2f}
+        Please investigate and retrain the model if necessary.'''
         
-        recipients = "aleksander.babij@gmail.com"
+        recipients = ["aleksander.babij@gmail.com"]
         send_email(to=recipients, subject=subject, html_content=body)
+        
 
 
 with DAG(
@@ -42,17 +40,10 @@ with DAG(
         'model': 'model.pkl'
     }
 ) as dag:
-    
-    email_test_task = EmailOperator(
-        task_id='send_test_email',
-        to='aleksander.babij@gmail.com',
-        subject='Test Email from Airflow using AWS SES',
-        html_content='This is a test email sent using AWS SES SMTP configuration.',
+
+    monitor_model_task = PythonOperator(
+        task_id='monitor_model',
+        python_callable=monitor_model,
     )
 
-    # monitor_model_task = PythonOperator(
-    #     task_id='monitor_model',
-    #     python_callable=monitor_model,
-    # )
-
-    email_test_task
+    monitor_model_task
